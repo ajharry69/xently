dc27682d351ac756705418889d24e9b4a10c5490
Orinda Harrison <ajuongharry35@gmail.com>
2022-02-10 18:00:31 +0300

    Log SQL queries in when in debug mode

--- data/source/local/src/main/java/co/ke/xently/source/local/di/StorageModule.kt
+++ data/source/local/src/main/java/co/ke/xently/source/local/di/StorageModule.kt
@@ -2,6 +2,7 @@ package co.ke.xently.source.local.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.room.Room
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
@@ -9,25 +10,36 @@ import co.ke.xently.common.ENCRYPTED_SHARED_PREFERENCE_KEY
 import co.ke.xently.common.UNENCRYPTED_SHARED_PREFERENCE_KEY
 import co.ke.xently.common.di.qualifiers.EncryptedSharedPreference
 import co.ke.xently.common.di.qualifiers.UnencryptedSharedPreference
+import co.ke.xently.common.isReleaseBuild
 import co.ke.xently.source.local.Database
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.util.concurrent.Executors
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object StorageModule {
+    private val TAG: String = StorageModule::class.java.simpleName
+
     @Provides
     @Singleton
     fun provideDatabase(@ApplicationContext context: Context): Database {
-        return Room.databaseBuilder(
-            context.applicationContext,
-            Database::class.java,
-            "xently.db",
-        ).fallbackToDestructiveMigration().build()
+        return Room.databaseBuilder(context, Database::class.java, "xently.db")
+            .fallbackToDestructiveMigration()
+            .apply {
+                if (!isReleaseBuild()) {
+                    setQueryCallback(
+                        { query, args ->
+                            Log.d(TAG, "Query <${query}>. Args: <${args.joinToString()}>")
+                        },
+                        Executors.newSingleThreadExecutor(),
+                    )
+                }
+            }.build()
     }
 
     @Provides
