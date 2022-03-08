21047634d13be0e68f61c42bf78c425a00e9eb2b
Orinda Harrison <ajuongharry35@gmail.com>
2022-03-08 22:37:38 +0300

    Filter local shopping lists by date added

--- feature/shoppinglist/src/main/java/co/ke/xently/shoppinglist/repository/ShoppingListRepository.kt
+++ feature/shoppinglist/src/main/java/co/ke/xently/shoppinglist/repository/ShoppingListRepository.kt
@@ -3,6 +3,7 @@ package co.ke.xently.shoppinglist.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.map
+import co.ke.xently.common.DEFAULT_SERVER_DATE_FORMAT
 import co.ke.xently.common.Retry
 import co.ke.xently.data.*
 import co.ke.xently.feature.repository.Dependencies
@@ -127,8 +128,16 @@ internal class ShoppingListRepository @Inject constructor(private val dependenci
 
     override fun get(config: PagingConfig, group: ShoppingListGroup?) = Pager(
         config = config,
-        remoteMediator = ShoppingListRemoteMediator(group,dependencies),
-        pagingSourceFactory = dependencies.database.shoppingListDao::get,
+        remoteMediator = ShoppingListRemoteMediator(group, dependencies),
+        pagingSourceFactory = {
+            if (group != null && group.groupBy == DateAdded) {
+                dependencies.database.shoppingListDao.get(
+                    DEFAULT_SERVER_DATE_FORMAT.parse(group.group.toString())!!,
+                )
+            } else {
+                dependencies.database.shoppingListDao.get()
+            }
+        },
     ).flow.map { data ->
         data.map { it.item }
     }
